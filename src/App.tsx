f3ed3450faa91af49c3cb6f885068211b1bfc99f
Oliver Beck <o.oliver.beck@gmail.com>
2023-05-05 11:58:22 +0200

    added "zustand" for state management
     - updated model to be represented in two stores, an "AppStore" and an "AccommodationStore"
     - display mock data in a list in a new "overview" component that is displayed in the app component
     - added buttons to overview component that lead to an "Accommodation" component. It shows the selected accommodation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Accommodation, accommodations } from './mock-data';
-import Overview from './overview';
+import Overview from './components/Overview/Overview';
+import Accommodation from './components/Accommodation/Accommodation';
+import { AccommodationState, AppState, IAccommodation, useAccommodationStore, useAppStore } from './App.store';
+import { useEffect } from 'react';
 
 const App: any = () => {
+
+  const location = useAppStore((state: AppState) => state.location);
+  const changeLocation = useAppStore((state: AppState) => state.updateLocation);
+
+  const accommodations = useAccommodationStore((state: AccommodationState) => state.accommodations);
+  const fetchAccommodations = useAccommodationStore((state: AccommodationState) => state.getAccommodations)
+  const selectAccommodation = useAccommodationStore((state: AccommodationState) => state.selectAccommodation)
+
+  function handleAccommodationButtonClicked(id: string) {
+    selectAccommodation(id);
+    changeLocation('accommodation');
+  }
+
+  useEffect(() => {
+    fetchAccommodations();
+  },[])
+
   return (
     <div className="App">
-      <Overview accommodations={accommodations} />
+      <button onClick={() => { changeLocation('overview') }}>home</button>
+      {location === 'overview'
+        ? <Overview accommodations={accommodations} onButtonClick={(id: string) => handleAccommodationButtonClicked(id)} />
+        : <Accommodation selectedAccommodation={accommodations.filter((accommodation: IAccommodation) => {
+          return accommodation.selected;
+        })[0]}/>
+      }
     </div>
   );
 }
 
 export default App;
-
-export interface AccommodationsProps {
-  accommodations: Accommodation[];
-}
